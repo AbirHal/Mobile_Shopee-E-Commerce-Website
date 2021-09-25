025e7795d6c6bd129cd8c83a92800a265ffe9db4
abir hal <77001053+AbirHal@users.noreply.github.com>
2021-09-25 14:55:39 +0100

    Update shopee.sql

--- database/shopee.sql
+++ database/shopee.sql
@@ -19,54 +19,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `shopee`
+-- Database: `shopping_site`
 --
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
-
-CREATE TABLE `cart` (
-  `cart_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product`
---
-
-CREATE TABLE `product` (
-  `item_id` int(11) NOT NULL,
-  `item_brand` varchar(200) NOT NULL,
-  `item_name` varchar(255) NOT NULL,
-  `item_price` double(10,2) NOT NULL,
-  `item_image` varchar(255) NOT NULL,
-  `item_register` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_price`, `item_image`, `item_register`) VALUES
-(1, 'Samsung', 'Samsung Galaxy 10', 152.00, './assets/products/1.png', '2020-03-28 11:08:57'), -- NOW()
-(2, 'Redmi', 'Redmi Note 7', 122.00, './assets/products/2.png', '2020-03-28 11:08:57'),
-(3, 'Redmi', 'Redmi Note 6', 122.00, './assets/products/3.png', '2020-03-28 11:08:57'),
-(4, 'Redmi', 'Redmi Note 5', 122.00, './assets/products/4.png', '2020-03-28 11:08:57'),
-(5, 'Redmi', 'Redmi Note 4', 122.00, './assets/products/5.png', '2020-03-28 11:08:57'),
-(6, 'Redmi', 'Redmi Note 8', 122.00, './assets/products/6.png', '2020-03-28 11:08:57'),
-(7, 'Redmi', 'Redmi Note 9', 122.00, './assets/products/8.png', '2020-03-28 11:08:57'),
-(8, 'Redmi', 'Redmi Note', 122.00, './assets/products/10.png', '2020-03-28 11:08:57'),
-(9, 'Samsung', 'Samsung Galaxy S6', 152.00, './assets/products/11.png', '2020-03-28 11:08:57'),
-(10, 'Samsung', 'Samsung Galaxy S7', 152.00, './assets/products/12.png', '2020-03-28 11:08:57'),
-(11, 'Apple', 'Apple iPhone 5', 152.00, './assets/products/13.png', '2020-03-28 11:08:57'),
-(12, 'Apple', 'Apple iPhone 6', 152.00, './assets/products/14.png', '2020-03-28 11:08:57'),
-(13, 'Apple', 'Apple iPhone 7', 152.00, './assets/products/15.png', '2020-03-28 11:08:57');
+INSERT INTO `product` (`item_id`, `item_name`, `item_price`, `item_image`) VALUES
+(1, 'collaction', 150.00, './images/3.jpg'), -- NOW()
+
+(1, 'collaction', 150.00, './images/pic9.jpg'),
+(1, 'to do list', 50.00, './images/pic13.jpg'),
+(1, 'planner', 100.00, './images/pic3.jpg'),
+(1, 'sticky note', 50.00, './images/pic5.jpg'),
+(1, 'sticky note', 50.00, './images/pic6.jpg'),
+(1, 'planner', 100.00,  './images/pic14.jpg'),
+(1, 'sticker', 50.00, './images/4298865.jpg'),
+(1, 'to do list', 50.00, './images/pic11.jpg'),
+(1,'to do list', 50.00, './images/pic10.jpg'),
+(1, 'sticky note', 50.00, './images/pic2.jpg'),
+(1, 'sticky note', 50.00, './images/pic4.jpg'),
+(1, 'sticky note', 50.00, './images/pic7.jpg'),
+(1, 'planner', 100.00, './images/pic12.jpg'),
+(1,  'sticker', 50.00, './images/4318459.jpg'),
+(13,  'to do list', 50.00, './images/pic8.jpg');
 
 -- --------------------------------------------------------
 
@@ -86,8 +65,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `register_date`) VALUES
-(1, 'Daily', 'Tuition', '2020-03-28 13:07:17'),
-(2, 'Akshay', 'Kashyap', '2020-03-28 13:07:17');
+(1, 'abir', 'haloudji', '2021-09-25 13:07:17'),
+(2, 'ramzi', 'halouadji', '2021-09-25 13:07:17');
 
 -- --------------------------------------------------------
 
@@ -105,11 +84,6 @@ CREATE TABLE `wishlist` (
 -- Indexes for dumped tables
 --
 
---
--- Indexes for table `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`cart_id`);
 
 --
 -- Indexes for table `product`
@@ -121,24 +95,16 @@ ALTER TABLE `product`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`cart_id`);
 
 --
+
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`item_id`);
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `cart`
---
-ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
 --
 -- AUTO_INCREMENT for table `user`
 --
